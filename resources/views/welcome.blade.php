9c5f7cf40cfab077b6d9194879b8ae26ffabb8d2
prisMolina <a6519150038@utch.edu.mx>
2022-03-26 17:52:22 -0700

    Cambiecilos en titulos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
                 <div class="row gx-5 justify-content-center">
                     <div class="col-lg-6">
                         <div class="text-center my-5">
-                            <h1 class="display-5 fw-bolder text-white mb-2">La prevención es el primer paso</h1>
+                            <h1 class="display-5 fw-bolder text-white mb-2">Prevenir el es primer paso</h1>
                             <p class="lead text-white-50 mb-4">La presencia de CO2 en espacios cerrados afecta el rendimiento laboral y escolar, medir la concentración de este es el primer paso hacia la mejora de calidad del aire en espacios cerrados</p>
                         </div>
                     </div>
@@ -18,12 +18,12 @@
                 <div class="row gx-5">
                     <div class="col-lg-4 mb-5 mb-lg-0">
                         <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-collection"></i></div>
-                        <h2 class="h4 fw-bolder">Dato sobre CO2</h2>
-                        <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
+                        <h2 class="h4 fw-bolder">Efectos fisicos</h2>
+                        <p>Físicamente una persona que esta expuesta a concentraciones de mas 800ppm de CO2 puede sentir dolor de cabeza, mareos, e incluso dificultad para respirar.</p>
                     </div>
                     <div class="col-lg-4 mb-5 mb-lg-0">
                         <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-building"></i></div>
-                        <h2 class="h4 fw-bolder">Dato sobre CO2</h2>
+                        <h2 class="h4 fw-bolder">Efectos cognitivos</h2>
                         <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
                     </div>
                     <div class="col-lg-4">
